8786fde2a3ee976476a8c89c103b1817c4b060ea
Thalisu <thalisondmcc@gmail.com>
2025-01-27 19:23:19 -0300

    feat: Add Transfers collection to AppUser model for managing user transfer records

--- app/Models/AppUser.cs
+++ app/Models/AppUser.cs
@@ -9,5 +9,6 @@ namespace app.Models
     public class AppUser : IdentityUser
     {
         public Wallet Wallet { get; set; } = null!;
+        public ICollection<Transfer> Transfers { get; set; } = [];
     }
 }
