5fa788d22084d5679a2770a175baa49a2a1fa883
Thalisu <thalisondmcc@gmail.com>
2025-01-27 13:46:57 -0300

    feat: Add AppUser model with Wallet property for user management

---
+++ app/Models/AppUser.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace app.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public Wallet Wallet { get; set; } = null!;
+    }
+}
