5bcc37adc37c2b9fdf9ed4137afb35f5be8cc16b
Thalisu <thalisondmcc@gmail.com>
2025-01-27 17:18:34 -0300

    feat: Add UserMapper for converting AppUser to UserDto

---
+++ app/Mappers/UserMapper.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using app.Dtos.User;
+using app.Models;
+
+namespace app.Mappers
+{
+    public static class UserMapper
+    {
+        public static UserDto ToUserDto(this AppUser user, string token)
+        {
+
+            if (user.Email == null || user.UserName == null)
+            {
+                throw new ArgumentNullException(user.Email, user.UserName);
+            }
+            return new UserDto
+            {
+                Email = user.Email,
+                Username = user.UserName,
+                Token = token
+            };
+        }
+    }
+}
