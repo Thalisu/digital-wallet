64bced2ce3b3df9ed9de848a422811640dbfa261
Thalisu <thalisondmcc@gmail.com>
2025-01-27 13:47:02 -0300

    feat: Add Wallet model with properties for BRL, USD, and user association

---
+++ app/Models/Wallet.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app.Models
+{
+    public class Wallet
+    {
+        [Column(TypeName = "decimal(18, 2)")]
+        public int BRL { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public int USD { get; set; }
+        public string UserId { get; set; } = null!;
+        public AppUser AppUser { get; set; } = null!;
+    }
+}
