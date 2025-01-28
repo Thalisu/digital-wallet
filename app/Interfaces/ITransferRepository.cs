4f23d4c3fafdf7bead0519ad63c71c62696d57d2
Thalisu <thalisondmcc@gmail.com>
2025-01-28 10:58:35 -0300

    feat: Update GetTransfersAsync method in ITransferRepository to accept query parameters for filtering transfers

--- app/Interfaces/ITransferRepository.cs
+++ app/Interfaces/ITransferRepository.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using app.Dtos.Transfer;
+using app.helpers;
 using app.Models;
 
 namespace app.Interfaces
 {
     public interface ITransferRepository
     {
-        Task<List<TransferDto>> GetTransfersAsync(AppUser user);
+        Task<List<TransferDto>> GetTransfersAsync(AppUser user, QueryObject queryObject);
         Task<TransferDto> CreateTransferAsync(CreateTransferDto transferDto);
     }
 }
--- app/Repository/TransferRepository.cs
+++ app/Repository/TransferRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using app.Data;
 using app.Dtos.Transfer;
+using app.helpers;
 using app.Interfaces;
 using app.Mappers;
 using app.Models;
@@ -15,11 +16,13 @@ namespace app.Repository
         : ITransferRepository
     {
         private readonly ApplicationDBContext _context = context;
-        async public Task<List<TransferDto>> GetTransfersAsync(AppUser user)
+        async public Task<List<TransferDto>> GetTransfersAsync(AppUser user, QueryObject queryObject)
         {
-            var transfers = await _context.Transfers
-                .Where(t => t.AppUserId == user.Id)
-                .ToListAsync();
+            var transfers = await _context.Transfers.Where(
+                t => t.AppUserId == user.Id
+                     && t.Date >= queryObject.FrDate
+                      && t.Date <= queryObject.ToDate).ToListAsync();
+
             return [.. transfers.Select(t => t.ToTransferDto())];
         }
         async public Task<TransferDto> CreateTransferAsync(CreateTransferDto transferDto)
