f4c7a809e86722da4cd68e92e5ae0299822eacd9
Thalisu <thalisondmcc@gmail.com>
2025-01-28 11:32:25 -0300

    feat: Add Id property to UserDto for user identification

--- app/Dtos/User/UserDto.cs
+++ app/Dtos/User/UserDto.cs
@@ -7,6 +7,7 @@ namespace app.Dtos.User
 {
     public class UserDto
     {
+        public string Id { get; set; } = "";
         public string Username { get; set; } = "";
         public string Email { get; set; } = "";
         public string Token { get; set; } = "";
